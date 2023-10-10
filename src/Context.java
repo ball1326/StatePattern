177f45285e40d0176fd91007b6b45e01f2bcd7fc
ball1326 <aticha13264@gmail.com>
2023-10-10 20:44:00 +0700

    first commit

---
+++ src/Context.java
@@ -0,0 +1,13 @@
+
+class Context {
+    private State state;
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void execute() {
+        state.doAction();
+        state.doAction2();
+    }
+}
